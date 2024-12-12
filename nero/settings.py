1d164d0cb90ddef049778d14685e4dc4886ec2c5
AlexsandroCosta <alexsandro.costasv@gmail.com>
2024-12-12 10:25:35 -0300

    config cors

--- nero/settings.py
+++ nero/settings.py
@@ -115,7 +115,29 @@ LOGGING = {
     },
 }
 
+CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
+
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
 
 CORS_ALLOWED_ORIGINS = [
     'https://api.nero.lat',

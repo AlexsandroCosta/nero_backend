c0da4d1560a301d8e2c76ab8ac8a2ff5462fb863
PreciousMy <alvaromaiachaves@gmail.com>
2025-01-14 17:34:58 -0300

    sexooooooo

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+# class cadastroTeste(TestCase):
+#     def setUp(self):
+#         self.url = '/usuario/'
+#         self.valid_data = {
+#         "username": "ea7FPFCyeeslI@89.sLkq2sCJoukrsPVm-I-SlHHqy4p@lV2",
+#         "password": "string",
+#         "first_name": "string",
+#         "email": "user@example.com",
+#         "sexo": "m",
+#         "cpf": "string",
+#         "grau_ensino": "1",
+#         "data_nascimento": "2025-01-14"
+#         }
+#         self.invalid_data = {
+#         "username": "ea7FPFCyeeslI@89.sLkq2sCJoukrsPVm-I-SlHHqy4p@lV2",
+#         "password": "string",
+#         "first_name": "string",
+#         "email": "user@example.com",
+#         "sexo": "m",
+#         "cpf": "string",
+#         "grau_ensino": "1",
+#         "data_nascimento": "2025-01-14"
+#         }
+
+#     def cadastroCerto(self):

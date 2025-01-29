d29c347eca078ada23c0355d6661fdd7a964d96e
AlexsandroCosta <alexsandro.costasv@gmail.com>
2025-01-29 02:16:24 -0300

    conf email

--- nero/settings.py
+++ nero/settings.py
@@ -143,3 +143,10 @@ CORS_ALLOWED_ORIGINS = [
     'https://api.nero.lat',
     'https://localhost:8000',
 ]
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'lulluu1554@gmail.com'
+EMAIL_HOST_PASSWORD = 'gwhe rupn ohzb rryw'

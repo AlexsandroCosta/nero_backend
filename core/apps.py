6e2620f5551de9e351e4b80160cb557cdd503378
Alexsandro Costa <108533432+AlexsandroCosta@users.noreply.github.com>
2025-02-05 13:49:00 -0300

    Update apps.py

--- core/apps.py
+++ core/apps.py
@@ -9,9 +9,23 @@ class CoreConfig(AppConfig):
         post_migrate.connect(povoar_db, sender=self)
 
 def povoar_db(sender, **kwargs):
-    from .models import Usuario
+    from .models import Usuario, Postagem, Bairro
+    from shapely.geometry import Point, Polygon
 
     if not Usuario.objects.filter(username='admin').exists():
         admin = Usuario.objects.create_superuser(username='admin')
         admin.set_password('admin')
         admin.save()
+
+    for postagem in Postagem.objects,filter(status='1'):
+        for bairro in Bairro.objects.all():
+            poligono = Polygon(bairro.pontos)
+
+            latitude, longitude = postagem.geolocalizacao.split(',')
+
+            ponto = Point(float(latitude), float(longitude))
+
+            if poligono.contains(ponto):
+                bairro.quantidade_reclamacoes+=1
+                bairro.save()
+                break
